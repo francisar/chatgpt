648debbdd9a1c9d43c15f77e09a6bf7754698970
francis.cui <francis.cui@shopee.com>
2023-07-06 11:38:19 +0800

    增加日志记录

--- ginserve/officialaccount_serve.go
+++ ginserve/officialaccount_serve.go
@@ -2,24 +2,27 @@ package ginserve
 
 import (
 	"fmt"
-	"log"
 	"github.com/francisar/chatgpt/handler"
 	"github.com/gin-gonic/gin"
 	"github.com/silenceper/wechat/v2"
 	"github.com/silenceper/wechat/v2/cache"
+	"github.com/silenceper/wechat/v2/officialaccount"
 	offConfig "github.com/silenceper/wechat/v2/officialaccount/config"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"net/http"
 )
 
-func OffServe(c *gin.Context)  {
-	wc := wechat.NewWechat()
+var wc * wechat.Wechat
+var officialAccount * officialaccount.OfficialAccount
+
+func init()  {
+	wc = wechat.NewWechat()
 	memory := cache.NewMemory()
 	appId := viper.GetString("WECHAT_APPID")
 	appSecret := viper.GetString("WECHAT_APPSECRET")
 	token := viper.GetString("WECHAT_TOKEN")
 	encodingAESKey := viper.GetString("WECHAT_ENCODINGAESKEY")
-	log.Debug(appId)
 	cfg := &offConfig.Config{
 		AppID:     appId,
 		AppSecret: appSecret,
@@ -27,10 +30,15 @@ func OffServe(c *gin.Context)  {
 		EncodingAESKey: encodingAESKey,
 		Cache: memory,
 	}
-	officialAccount := wc.GetOfficialAccount(cfg)
+	log.Debugf("appId:%s", appId)
+	officialAccount = wc.GetOfficialAccount(cfg)
+}
+
+func OffServe(c *gin.Context)  {
 	server := officialAccount.GetServer(c.Request, c.Writer)
 	// 设置接收消息的处理方法
 	server.SetMessageHandler(handler.OffAccMessageHandler)
+	log.Debugf("url:%s", c.Request.URL)
 	// 处理消息接收以及回复
 	err := server.Serve()
 	if err != nil {
